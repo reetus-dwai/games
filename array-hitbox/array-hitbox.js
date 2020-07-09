9a17fa720ebcea628a45835389ab1cb53245e0bf
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-07-09 14:06:12 -0600

    Update array-hitbox.js

--- array-hitbox/array-hitbox.js
+++ array-hitbox/array-hitbox.js
@@ -4,14 +4,16 @@ function writeField(x, y){
   //add x values
   for(var i = 0; i < x; i++){
     arr.push([]);
+    console.log('x: ' + i);
     
     //add y values
     for(var k = 0; k < y; k++){
       arr[i].push(0);
-      console.log(i);
+      console.log('y: ' + i);
       console.log(arr[i]);
     }
   }
+  console.log(arr);
   return arr;
 }
 function drawField(field, target){
