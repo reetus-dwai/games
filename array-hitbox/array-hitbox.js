8ef49bb466fce01261aea84d2d08edac6be73414
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-07-09 13:58:24 -0600

    Create array-hitbox.js

---
+++ array-hitbox/array-hitbox.js
@@ -0,0 +1,18 @@
+function writeField(x, y){
+  let arr = [];
+  
+  //add x values
+  for(var i = 0; i <= x; i++){
+    arr.push([]);
+    
+    //add y values
+    for(var k = 0; k <= y; k++){
+      arr[i].push();
+    }
+  }
+  return arr;
+}
+function drawField(field, target){
+  let tgt = document.getElementById(target);
+  
+}
