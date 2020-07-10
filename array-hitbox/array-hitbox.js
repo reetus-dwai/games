f3d3d8d13f604ba3bb6376771e2c12d0985413d9
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-07-09 18:49:04 -0600

    Update array-hitbox.js

--- array-hitbox/array-hitbox.js
+++ array-hitbox/array-hitbox.js
@@ -13,7 +13,23 @@ function writeField(x, y){
   console.log(arr);
   return arr;
 }
-function drawField(field, target){
-  let tgt = document.getElementById(target);
+function drawField(field, target, newId){
+  let table = document.createElement('table');
+  table.id = newId;
+  document.getElementById(target).appendChild(table);
   
+  let rows = document.createElement('tr');
+  //let rowNum = 0;
+  let values = document.createElement('td');
+  //let valueNum = 0;
+  values.style.border = '1px solid black';
+  
+  for(var i = 0; i <= field.length; i++){
+    rows.id = i.toString();
+    document.getElementById(newId).appendChild(rows);
+    
+    for(var k = 0; k <= field[0].length; k++){
+      document.getElementById(i.toString()).appendChild(values);
+    }
+  }
 }
